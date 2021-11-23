fbaf5f285e4b94083b00c9cc742733321ef47f32
Abanoub Samir <abanoubsamirshenouda@gmail.com>
2021-11-23 23:54:45 +0200

    add logo and title to website tab

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle'
-import Footer from './Components/SharedComponents/Footer.jsx'
-import NavBar from './Components/SharedComponents/NavBar.jsx'
-import Home from './Components/home/Home';
-import Signin from './Components/Forms/Signin/Signin.jsx'
-import Signup from './Components/Forms/signup/Signup.jsx'
-import UserProfile from './Components/UserProfile/UserProfile.jsx';
-import NotFound from './Components/SharedComponents/PageNotFound/NotFound.jsx'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './App.css';
-import GetBooks from './Components/BookSearch/GetBooks.jsx';
-import DisplayCom from './Components/BookSearch/DisplayCom.jsx';
-import BookCard from './Components/BookSearch/BookCard.jsx';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+import Footer from "./Components/SharedComponents/Footer.jsx";
+import NavBar from "./Components/SharedComponents/NavBar.jsx";
+import Home from "./Components/home/Home";
+import Signin from "./Components/Forms/Signin/Signin.jsx";
+import Signup from "./Components/Forms/signup/Signup.jsx";
+import UserProfile from "./Components/UserProfile/UserProfile.jsx";
+import NotFound from "./Components/SharedComponents/PageNotFound/NotFound.jsx";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import GetBooks from "./Components/BookSearch/GetBooks.jsx";
+import DisplayCom from "./Components/BookSearch/DisplayCom.jsx";
+import BookCard from "./Components/BookSearch/BookCard.jsx";
 // import getData, { BookAction } from './Store/bookProfile/BookAction'
 // import BookProfile from './Components/bookProfile/BookProfile'
 function App() {
@@ -19,13 +19,13 @@ function App() {
     <Router>
       <NavBar />
       <Switch>
-        <Route exact path='/' component={Home} />
-        <Route path='/userprofile' component={UserProfile}/>
-        <Route path='/notfound' component={NotFound}/>
-        <Route path='/login' component={Signin} />
-        <Route path='/register' component={Signup} />
-        <Route path='/getbooks' component={GetBooks} />
-        <Route path='/card' component={BookCard} />
+        <Route exact path="/" component={Home} />
+        <Route path="/userprofile" component={UserProfile} />
+        <Route path="/notfound" component={NotFound} />
+        <Route path="/login" component={Signin} />
+        <Route path="/register" component={Signup} />
+        <Route path="/getbooks" component={GetBooks} />
+        <Route path="/card" component={BookCard} />
         <Route path="/:details_id" component={DisplayCom} />
       </Switch>
       <Footer />
