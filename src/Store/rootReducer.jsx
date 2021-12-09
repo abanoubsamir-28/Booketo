eb9edcd6352ffcdb08f07a161f6aad3c2d33102a
Abanoub Samir <abanoubsamirshenouda@gmail.com>
2021-12-09 21:49:28 +0200

    user profile final

--- src/Store/rootReducer.jsx
+++ src/Store/rootReducer.jsx
@@ -3,10 +3,12 @@ import { combineReducers } from 'redux'
 import CreatePostReducer from './createPost/createPostReducer'
 import cartStoreReducer from './CartStore/cartStoreReducer'
 import { WishListReducer } from './wishList/WishListReducer';
+import {getPostsReducer} from './getPosts/getPostsReducer'
 const rootReducer = combineReducers({
     createPost: CreatePostReducer,
     cartStore: cartStoreReducer,
-    wishList:WishListReducer
+    wishList:WishListReducer,
+    getPosts:getPostsReducer
 })
 
 export default rootReducer;
